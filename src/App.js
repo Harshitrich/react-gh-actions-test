ed3b9cf8a74630dcb425492fbccfa6b4ee684c33
Harshitrich <harshit.richariya@katonic.ai>
2021-03-15 04:23:03 +0530

    Test commit msg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +15,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      <h1>Hello, World!</h1>
+	</header>
     </div>
   );
 }
